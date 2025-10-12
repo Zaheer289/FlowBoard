08284797baa56dd28248f18898d7eaa369fec5b8
Zack <itszaheer289@gmail.com>
2025-10-12 02:10:33 +0100

    Added comment and project schemas:

--- flowboard-backend/src/models/User.js
+++ flowboard-backend/src/models/User.js
@@ -5,10 +5,10 @@ const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
     passwordHash: {type: String, required:true},
     profilePicture: {type:String},
-    projects:[{type: mongoose.Schema.Types.ObjectId, ref:"Project"}],
-    sharedProjects:[{type: mongoose.Schema.Types.ObjectId, ref:"Project"}],
-    likedProjects:[{type: mongoose.Schema.Types.ObjectId, ref:"Project"}],
-    savedProjects:[{type: mongoose.Schema.Types.ObjectId, ref:"Project"}],
+    projects: [{type: mongoose.Schema.Types.ObjectId, ref:"Project"}],
+    sharedProjects: [{type: mongoose.Schema.Types.ObjectId, ref:"Project"}],
+    likedProjects: [{type: mongoose.Schema.Types.ObjectId, ref:"Project"}],
+    savedProjects: [{type: mongoose.Schema.Types.ObjectId, ref:"Project"}],
 },{timestamps: true});
 
 const User = mongoose.model("User",userSchema);
