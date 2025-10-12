aa64fc29349424c2571e890e876ee18ff8785ca0
Zack <itszaheer289@gmail.com>
2025-10-12 12:41:55 +0100

    Added verifyPassword function in the user schema

--- flowboard-backend/src/models/User.js
+++ flowboard-backend/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -34,6 +35,10 @@ const userSchema = new mongoose.Schema({
         ref: "Project"
     }],
 },{timestamps: true});
+
+userSchema.methods.verifyPassword = async function (password) {
+    return bcrypt.compare(password, this.passworddHash);
+};
 
 const User = mongoose.model("User",userSchema);
 export default User
